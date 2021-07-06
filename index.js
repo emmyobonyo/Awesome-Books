5e24ad1d5bfb562768f4e28ba272edb9f988d804
yahyaelganyni1 <crazy20046@gmail.com>
2021-07-06 17:20:26 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,19 +4,18 @@ const form = document.querySelector('.form');
 const titleInput = document.querySelector('.title-input');
 const authorInput = document.querySelector('.author-input');
 const addBtn = document.querySelector('.add-btn');
-const removeBtn = document.createElement('button');
-const li = document.createElement('li');
 
-let books = [];
-
+// let books = [];
 function Book(title, author) {
   this.title = title;
   this.author = author;
   this.addBook = function () {
     form.addEventListener('submit', function (e) {
       e.preventDefault();
+      const li = document.createElement('li');
       const title = document.createElement('p');
       const author = document.createElement('p');
+      const removeBtn = document.createElement('button');
       title.innerHTML = titleInput.value;
       author.innerHTML = authorInput.value;
       removeBtn.innerHTML = 'Remove';
@@ -24,21 +23,18 @@ function Book(title, author) {
       li.appendChild(author);
       li.appendChild(removeBtn);
       bookList.appendChild(li);
-      localStorage.setItem('book', JSON.stringify(books));
+      // localStorage.setItem('book', JSON.stringify(books));
       titleInput.value = '';
       authorInput.value = '';
-    });
-  }
-  this.removeBook = function() {
-    removeBtn.addEventListener('click', function () {
-      bookList.removeChild(li);
-      localStorage.removeItem('book');
+      removeBtn.addEventListener('click', function () {
+        bookList.removeChild(li);
+        localStorage.removeItem('book');
+      });
     });
   };
 }
 textBook = new Book(titleInput, authorInput);
 textBook.addBook();
-textBook.removeBook();
 
 // let localList = JSON.parse(localStorage.getItem('book'));
 
