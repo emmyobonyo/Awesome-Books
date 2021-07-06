add0bde0f73b648fcf6c5a09b01d66c5eafbe134
yahyaelganyni1 <crazy20046@gmail.com>
2021-07-06 16:48:47 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ const form = document.querySelector('.form');
 const titleInput = document.querySelector('.title-input');
 const authorInput = document.querySelector('.author-input');
 const addBtn = document.querySelector('.add-btn');
+let books = [];
 
 function Book(title, author) {
   this.title = title;
   this.author = author;
-  this.addBook = function() {
+  this.addBook = function () {
     form.addEventListener('submit', function (e) {
       e.preventDefault();
       const li = document.createElement('li');
@@ -22,6 +23,7 @@ function Book(title, author) {
       li.appendChild(author);
       li.appendChild(removeBtn);
       bookList.appendChild(li);
+      localStorage.setItem('book', JSON.stringify(books));
       titleInput.value = '';
       authorInput.value = '';
       removeBtn.addEventListener('click', function () {
@@ -29,13 +31,11 @@ function Book(title, author) {
         localStorage.removeItem('book');
       });
     });
-  }
+  };
 }
 textBook = new Book(titleInput, authorInput);
 textBook.addBook();
 
-
-// let books = [];
 // let localList = JSON.parse(localStorage.getItem('book'));
 
 // if (localStorage.length > 0) {
